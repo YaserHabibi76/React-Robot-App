46817210f4e19f23d266df7c81e15e8b32cd2282
yhabibi <yhabibi@uoguelph.ca>
2020-07-03 11:46:44 -0400

    Updates

---
+++ precache-manifest.736468b81532d9ecd46896dc581c5b6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bd65327de5ac633cfcfe6124d5775a",
+    "url": "/React-Robot-App/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/React-Robot-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "725286bb21c97ebaa5e3",
+    "url": "/React-Robot-App/static/css/main.881717a6.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/React-Robot-App/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Robot-App/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725286bb21c97ebaa5e3",
+    "url": "/React-Robot-App/static/js/main.ba509d78.chunk.js"
+  },
+  {
+    "revision": "aa0ec5b7608a79241f1a",
+    "url": "/React-Robot-App/static/js/runtime-main.8ffb36f1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-Robot-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
